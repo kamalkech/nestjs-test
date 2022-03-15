7f11e6d07902bede2f4437b4adbd99454b79c61d
kamalkech <maroc.develop@gmail.com>
2022-03-15 23:25:14 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   // const secret = csurf.tokens.secretSync();
   // const token = csurf.create(secret)
@@ -13,16 +22,28 @@ async function bootstrap() {
 
   // app.use(csurf());
 
-  // const options = new DocumentBuilder()
-  //   .setTitle('API example')
-  //   .setDescription('The API description')
-  //   .setVersion('1.0')
-  //   .addTag('nestjs')
-  //   .addBearerAuth()
-  //   .build();
+  const options = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .addBearerAuth()
+    .build();
 
-  // const swaggerDoc = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('v1/api', app, swaggerDoc);
+  const swaggerDoc = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('v1/api', app, swaggerDoc);
+
+  //
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('handlebars'),
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
 
   const port = process.env.PORT;
   await app.listen(port);
